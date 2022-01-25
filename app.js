3cf17ee97fc3e0a99a42da162b369e40bbae4fcb
Joshua DiFrancesco <joshiqua@gmail.com>
2022-01-25 10:55:29 -0500

    added all files to develop

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => { //Name of the file, data that will be written on that file 
+  if (err) throw err; //callback function for error handeling
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});   // `` backticks reference
+
