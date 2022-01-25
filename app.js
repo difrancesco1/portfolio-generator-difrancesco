cdbc50bae614a034425f4ad1f0b4733421306c3a
Joshua DiFrancesco <joshiqua@gmail.com>
2022-01-25 16:04:56 -0500

    added all

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+// const pageHTML = generatePage(name, github);
 
-const profileDataArgs = process.argv.slice(2);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-console.log(profileDataArgs);
-
-const [name, github] = profileDataArgs;
-
-console.log(name, github);
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', pageHTML, err => { //Name of the file, data that will be written on that file 
-  if (err) throw err; //callback function for error handeling
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});   // `` backticks reference
-
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+  // `` backticks reference
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
